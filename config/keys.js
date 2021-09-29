21c70cf9fc988f6cebfcc909bc81e5b5ace08493
unknown <mahelidutta12@gmail.com>
2021-09-29 16:31:18 +0530

    twiiterbackend

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://maheli:maheli@cluster0-shard-00-00.7zz05.mongodb.net:27017,cluster0-shard-00-01.7zz05.mongodb.net:27017,cluster0-shard-00-02.7zz05.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-3sztak-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secretOrKey: 'secret'
+};
